d02185cf3bcea1841bbb80a161d25995915f68be
Carlos <carlos.araya@gmail.com>
2020-02-14 22:01:59 -0800

    Initial push

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const gulp = require('gulp');
+const postcss = require('gulp-postcss');
+const postcssPresetEnv = require('postcss-preset-env');
+const sorting = require('postcss-sorting');
+const autoprefixer = require('autoprefixer');
+// const nano = require('cssnano');
+
+gulp.task('css', () => {
+  return gulp.src('./src/*.css').pipe(
+    postcss([
+      postcssPresetEnv({
+        stage: 3,
+        features: {
+          'nesting-rules': true
+        }
+      }),
+      sorting({
+        "properties-order": [
+          "font",
+          "color",
+          "margin",
+          "padding",
+          "border",
+          "background"
+        ]
+      }),
+      autoprefixer(/* no options needed*/)
+      // ,
+      // nano(/* pluginOptions */)
+    ])
+  ).pipe(
+    gulp.dest('./dist/css')
+  )
+});
